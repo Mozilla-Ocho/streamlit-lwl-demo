a2d274e5b80afccf565dfe0fe68f4f38ba3e9d57
Josh Whiting <joshwhiting@gmail.com>
2024-07-23 13:11:28 -0600

    improving the workflow and UX so that the whole quiz (all sections) care usable. generate each quiz section when the UX needs it, add more verbal context to the page, etc.

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import datetime
-from pipeline import run_pipeline
+from pipeline import generate_key_topics, generate_learning_context, generate_questions
 import json
 from streamlit_inspector import inspect
 
 st.set_page_config(page_title="Streamlit Chat App", page_icon="🧊", layout="wide")
 
-
-st.title("Learning Companion Concept Explorer")
+st.title("Learning with LLMs Concept Explorer")
 
 if "openai_model" not in st.session_state:
     st.session_state.model = "gpt-4o"
@@ -19,6 +18,14 @@ if 'skills' not in st.session_state:
     st.session_state.skills = ''
 if 'source_material' not in st.session_state:
     st.session_state.source_material = ''
+if 'quiz' not in st.session_state:
+    st.session_state.quiz = {"keyTopics":[]}
+if "key_topics" not in st.session_state:
+    st.session_state.key_topics = []
+if "quiz_active_topic_idx" not in st.session_state:
+    st.session_state.quiz_active_topic_idx = 0
+if "learning_context" not in st.session_state:
+    st.session_state.learning_context = ''
 
 col1, col2 = st.columns([1,3])
 
@@ -32,40 +39,91 @@ with col1:
         st.subheader("Source Material")
         source_material = st.text_area("Enter the source material here. Using Firefox's reader view on a web page is recommended, then just copy+paste:", key='source_material')
         submit_source_material = st.form_submit_button("Submit")
-    
-def quiz_ui():
+  
+def quiz_questions():
+    s_idx = st.session_state.quiz_active_topic_idx
+    quiz = st.session_state.quiz
+    section = quiz["keyTopics"][s_idx] if s_idx < len(quiz["keyTopics"]) else None
+    if not section:
+        return
+    for q_idx, question in enumerate(section["questions"]):
+        with st.expander(question["questionTitle"]):
+            options = question["answers"]
+            correct_index = question["answerIndex"]
+            with st.form(key=f"question_{s_idx}_{q_idx}"):
+                selected_index = st.radio("Select an answer:", options, key=f"answer_{s_idx}_{q_idx}")
+                submitted = st.form_submit_button("Submit")
+                if submitted:
+                    if options.index(selected_index) == correct_index:
+                        st.success("Correct! 🟢")
+                        st.markdown(f'**Why:** {question["reasoning"]}')
+                        st.markdown(f'**Relevance to your goals and skills:** {question["context_relevance"]}')
+                        st.markdown(f'**What to learn next:** {question["follow_up_knowledge"]}')
+                    else:
+                        st.error("Incorrect. 🔴")
+                        st.markdown(f'**Why:** {question["reasoning"]}')
+                        st.markdown(f'**Relevance to your goals and skills:** {question["context_relevance"]}')
+                        st.markdown(f'**What to review to get this right:** {question["requisite_knowledge"]}')
+
+def current_quiz_section():
+    key_topics = st.session_state.key_topics
+    if st.session_state.quiz_active_topic_idx >= len(key_topics):
+        st.session_state.quiz_active_topic_idx = 0
+    s_idx = st.session_state.quiz_active_topic_idx
+    active_topic = key_topics[s_idx]
+    col1,col2,col3 = st.columns([3,1,1])
+    with col1:
+        st.subheader(active_topic)
+    with col2:
+        if s_idx > 0:
+            if st.button("Previous topic"):
+                st.session_state.quiz_active_topic_idx -= 1
+                st.rerun()
+    with col3:
+        if s_idx < len(st.session_state.key_topics) - 1:
+            if st.button("Next topic"):
+                st.session_state.quiz_active_topic_idx += 1
+                st.rerun()
+    quiz = st.session_state.quiz
+    section = quiz["keyTopics"][s_idx] if s_idx < len(quiz["keyTopics"]) else None
+    if not section:
+        st.write(f"Generating questions for {active_topic}...")
+        source_material = st.session_state.source_material
+        openai_api_key = st.session_state.openai_api_key
+        model = st.session_state.model
+        learning_context = st.session_state.learning_context
+        questions = generate_questions(for_key_topic=active_topic, learning_context=learning_context, source_material=source_material, model=model, _openai_api_key=openai_api_key)
+        quiz["keyTopics"].append({"keyTopic": active_topic, "questions": questions})
+        st.session_state.quiz = quiz # do i need this?
+        st.rerun()
+    else:
+        quiz_questions()
+
+def quiz():
+    st.subheader("Initial context for the quiz:")
     placeholder = st.empty()
     goals = st.session_state.goals
     skills = st.session_state.skills
     source_material = st.session_state.source_material
     openai_api_key = st.session_state.openai_api_key
     model = st.session_state.model
-    [key_topics, learning_context, quiz] = run_pipeline(goals=goals, skills=skills, source_material=source_material, model=model, openai_api_key=openai_api_key, placeholder=placeholder)
+
+    placeholder.write(f"Generating learning context...")
+    st.markdown("First we generate a *learning context* document based on your learning goals and current skills. It asks the LLM to think through a learning trajectory for you with sub goals, topics to focus on, and problems you might encounter:")
+    learning_context = generate_learning_context(goals=goals, skills=skills, model=model, _openai_api_key=openai_api_key)
+    st.session_state.learning_context = learning_context
+    st.expander("Learning Context generated :white_check_mark:").write(learning_context)
+
+    placeholder.write(f"Generating key topics...")
+    st.markdown("Next, we generate a set of key topics from the source material you provided to break the quiz into relevant sections:")
+    key_topics = generate_key_topics(source_material=source_material, model=model, _openai_api_key=openai_api_key)
+    st.session_state.key_topics = key_topics
+    st.expander("Key Topics generated :white_check_mark:").write(key_topics)
+
     placeholder.empty()
-    quiz_data = json.loads(quiz)
-    for s_idx, section in enumerate(quiz_data["keyTopics"]):
-        st.subheader(section["keyTopic"])
-        for q_idx, question in enumerate(section["questions"]):
-            with st.expander(question["questionTitle"]):
-                options = question["answers"]
-                correct_index = question["answerIndex"]
-                # Use a form to prevent automatic reruns when a selection is made
-                with st.form(key=f"question_{s_idx}_{q_idx}"):
-                    selected_index = st.radio("Select an answer:", options, key=f"answer_{s_idx}_{q_idx}")
-                    submitted = st.form_submit_button("Submit")
-                    if submitted:
-                        # Check if the selected answer is correct
-                        if options.index(selected_index) == correct_index:
-                            st.success("Correct! 🟢")
-                            st.write(question["reasoning"])
-                            st.write(question["context_relevance"])
-                            st.write(question["follow_up_knowledge"])
-                        else:
-                            st.error("Incorrect. 🔴")
-                            st.write(f"The correct answer is: {options[correct_index]}")
-                            st.write(question["reasoning"])
-                            st.write(question["context_relevance"])
-                            st.write(question["requisite_knowledge"])
+    st.markdown("Finally, we generate a set of questions based on each key topic, with each one contextualized for you personally depending on whether your answer was right or wrong:")
+    st.header("Quiz:")
+    current_quiz_section()
 
 with col2:
     goals = st.session_state.goals
@@ -75,5 +133,5 @@ with col2:
     if goals == '' or skills == '' or source_material == '' or openai_api_key == '':
         st.warning("Please fill in all fields.")
     else:
-        quiz_ui()
+        quiz()
 
--- pipeline.py
+++ pipeline.py
@@ -5,20 +5,11 @@ from prompts.key_topics import key_topics_prompt
 from prompts.learning_context import learning_context_prompt
 from prompts.quiz_topic_questions import questions_prompt
 
-def run_pipeline(*, goals, skills, source_material, model, openai_api_key, placeholder):
-    client = OpenAI(api_key=openai_api_key)
-    placeholder.write("Extracting key topics...")
-    key_topics = extract_key_topics(source_material=source_material, model=model, _client=client)
-    placeholder.write("Generating learning context...")
-    learning_context = generate_learning_context(goals=goals, skills=skills, model=model, _client=client)
-    placeholder.write("Generating quiz...")
-    quiz = generate_quiz(key_topics=key_topics, learning_context=learning_context, source_material=source_material, model=model, _client=client, _placeholder=placeholder)
-    return key_topics, learning_context, quiz
-
 @st.cache_data(persist="disk")
-def extract_key_topics(*, source_material, model, _client):
+def generate_key_topics(*, source_material, model, _openai_api_key):
+    client = OpenAI(api_key=_openai_api_key)
     prompt = key_topics_prompt(source_material = source_material)
-    response = _client.chat.completions.create(
+    response = client.chat.completions.create(
         model= model,
           response_format={ "type": "json_object" },
           messages=[
@@ -28,14 +19,15 @@ def extract_key_topics(*, source_material, model, _client):
         temperature=0
     )
     body = response.choices[0].message.content
-    with st.expander("key topics extracted :white_check_mark:"):
-        st.write(body)
-    return body
+    topics = json.loads(body)["topics"] # prompt schema asks for this key
+    topics.insert(0, "Overview") # insert "overview" as first topic
+    return topics
 
 @st.cache_data(persist="disk")
-def generate_learning_context(*, goals, skills, model, _client):
+def generate_learning_context(*, goals, skills, model, _openai_api_key):
+    client = OpenAI(api_key=_openai_api_key)
     prompt = learning_context_prompt(goals=goals, skills=skills)
-    response = _client.chat.completions.create(
+    response = client.chat.completions.create(
         model= model,
           messages=[
             {"role": "user", "content": prompt}
@@ -43,29 +35,13 @@ def generate_learning_context(*, goals, skills, model, _client):
         temperature=0.4
     )
     learning_context = response.choices[0].message.content
-    with st.expander("learning context generated :white_check_mark:"):
-        st.write(learning_context)
     return learning_context
 
-def generate_quiz(*, key_topics, learning_context, source_material, model, _client, _placeholder):
-    quiz = {"keyTopics": []}
-    parsed_topics = json.loads(key_topics)
-    parsed_topics["topics"].insert(0, "Overview") # insert an "overview" topic
-    parsed_topics["topics"] = parsed_topics["topics"][:2] # trim to just two topics for testing
-    for key_topic in parsed_topics["topics"]:
-        _placeholder.write(f"Generating questions for {key_topic}...")
-        questions = generate_questions(key_topics=key_topics, for_key_topic=key_topic, learning_context=learning_context, source_material=source_material, model=model, _client=_client)
-        parsed_questions = json.loads(questions)
-        quiz["keyTopics"].append({"keyTopic": key_topic, "questions": parsed_questions["questions"]})
-    body = json.dumps(quiz)
-    with st.expander("quiz json generated :white_check_mark:"):
-        st.write(body)
-    return body
-
 @st.cache_data(persist="disk")
-def generate_questions(*, key_topics, for_key_topic, learning_context, source_material, model, _client):
+def generate_questions(*, for_key_topic, learning_context, source_material, model, _openai_api_key):
+    client = OpenAI(api_key=_openai_api_key)
     prompt = questions_prompt(for_key_topic=for_key_topic, learning_context=learning_context, source_material=source_material)
-    response = _client.chat.completions.create(
+    response = client.chat.completions.create(
         model= model,
           response_format={ "type": "json_object" },
           messages=[
@@ -75,6 +51,7 @@ def generate_questions(*, key_topics, for_key_topic, learning_context, source_ma
           #max_tokens= 4096,
         temperature=0.3
     )
-    json = response.choices[0].message.content
-    return json
+    body = response.choices[0].message.content
+    questions = json.loads(body)["questions"]
+    return questions
 
